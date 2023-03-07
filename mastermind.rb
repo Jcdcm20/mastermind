412bc30c7bb749bc85d93a9a725095e71b5d5982
Jcdcm20 <jcdcm20@gmail.com>
2023-03-07 11:10:42 -0300

    Add make_guess()

--- mastermind.rb
+++ mastermind.rb
@@ -4,4 +4,21 @@ def select_code(colors)
   return Array.new(4) { colors.sample }
 end
 
+def make_guess()
+  guess = []
+  i = 1
+  while i <= 4
+    puts "Enter color #{i}"
+    color = gets.capitalize
+    guess.push(color)
+  
+    i += 1
+  end
+
+  return guess
+end
+
 code = select_code(colors)
+guess = make_guess()
+
+puts guess
